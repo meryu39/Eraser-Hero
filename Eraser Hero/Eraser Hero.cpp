6ab8a750a8a3f93513c2a83fd0868e9325aa57fe
jackpot1022 <shin_1214@naver.com>
2023-11-06 23:34:48 +0900

    using namespace std
    까먹고 안해놨어요

--- Eraser Hero/Eraser Hero.cpp
+++ Eraser Hero/Eraser Hero.cpp
@@ -1,42 +1,49 @@
 ﻿#include <SDL.h>
 #include <SDL_image.h>
 #include <iostream>
+
+using namespace std;
+
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
+
 SDL_Window* gWindow = nullptr;
 SDL_Renderer* gRenderer = nullptr;
 SDL_Texture* gTexture = nullptr;
+
 bool initSDL() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
         return false;
     }
-    gWindow = SDL_CreateWindow("SDL Image Display", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    gWindow = SDL_CreateWindow("Eraser Hero", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (gWindow == nullptr) {
-        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
         return false;
     }
     gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
     if (gRenderer == nullptr) {
-        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << endl;
         return false;
     }
     SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
     int imgFlags = IMG_INIT_PNG;
     if (!(IMG_Init(imgFlags) & imgFlags)) {
-        std::cerr << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << std::endl;
+        cerr << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << endl;
         return false;
     }
     return true;
 }
-bool loadMedia(const std::string& imagePath) {
+
+bool loadMedia(const string& imagePath) {
     gTexture = IMG_LoadTexture(gRenderer, imagePath.c_str());
     if (gTexture == nullptr) {
-        std::cerr << "Failed to load image! SDL_image Error: " << IMG_GetError() << std::endl;
+        cerr << "Failed to load image! SDL_image Error: " << IMG_GetError() << endl;
         return false;
     }
     return true;
 }
+
 void closeSDL() {
     if (gTexture != nullptr) {
         SDL_DestroyTexture(gTexture);
@@ -53,13 +60,14 @@ void closeSDL() {
     IMG_Quit();
     SDL_Quit();
 }
+
 int main(int argc, char* args[]) {
     if (!initSDL()) {
-        std::cerr << "Failed to initialize SDL." << std::endl;
+        cerr << "Failed to initialize SDL." << endl;
         return -1;
     }
     if (!loadMedia("assets/temp_start.png")) {
-        std::cerr << "Failed to load media." << std::endl;
+        cerr << "Failed to load media." << endl;
         return -1;
     }
     bool quit = false;
@@ -72,7 +80,7 @@ int main(int argc, char* args[]) {
             else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_SPACE) {
                 // 스페이스 키를 누를 때 다음 화면으로 전환합니다.
                 if (!loadMedia("assets/temp_next.png")) {
-                    std::cerr << "Failed to load media." << std::endl;
+                    cerr << "Failed to load media." << endl;
                     return -1;
                 }
             }
