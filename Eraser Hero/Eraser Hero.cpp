c79f7ac30c28f3c11ff7a3a6a9e5a66d1923af48
meryu39 <62229467+meryu39@users.noreply.github.com>
2023-11-27 20:28:49 +0900

    플레이어 이동,충돌 구현

--- Eraser Hero/Eraser Hero.cpp
+++ Eraser Hero/Eraser Hero.cpp
@@ -1,94 +1,165 @@
 ﻿#include <SDL.h>
-#include <SDL_image.h>
 #include <iostream>
+#include <algorithm>
 
-using namespace std;
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+
+const int SCREEN_WIDTH = 1600;
+const int SCREEN_HEIGHT = 900;
+const int PLAYER_SIZE = 50;
+const int OBJECT_SIZE = 30;
+const int DASH_SPEED = 40;  // 돌진 속도
 
 SDL_Window* gWindow = nullptr;
 SDL_Renderer* gRenderer = nullptr;
-SDL_Texture* gTexture = nullptr;
+
+SDL_Rect playerRect = { SCREEN_WIDTH / 2 - PLAYER_SIZE / 2, SCREEN_HEIGHT / 2 - PLAYER_SIZE / 2, PLAYER_SIZE, PLAYER_SIZE };
+SDL_Rect objectRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, OBJECT_SIZE, OBJECT_SIZE };
+
+int playerHealth = 100;
+bool isColliding = false;
+
+bool isDashing = false;
+int dashDirectionX = 0;
+int dashDirectionY = 0;
 
 bool initSDL() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
+        std::cerr << "SDL 초기화 실패: " << SDL_GetError() << std::endl;
         return false;
     }
+
     gWindow = SDL_CreateWindow("Eraser Hero", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (gWindow == nullptr) {
-        cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
+        std::cerr << "윈도우 생성 실패: " << SDL_GetError() << std::endl;
         return false;
     }
+
     gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
     if (gRenderer == nullptr) {
-        cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << endl;
+        std::cerr << "렌더러 생성 실패: " << SDL_GetError() << std::endl;
         return false;
     }
-    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-    int imgFlags = IMG_INIT_PNG;
-    if (!(IMG_Init(imgFlags) & imgFlags)) {
-        cerr << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << endl;
-        return false;
-    }
-    return true;
-}
 
-bool loadMedia(const string& imagePath) {
-    gTexture = IMG_LoadTexture(gRenderer, imagePath.c_str());
-    if (gTexture == nullptr) {
-        cerr << "Failed to load image! SDL_image Error: " << IMG_GetError() << endl;
-        return false;
-    }
     return true;
 }
 
 void closeSDL() {
-    if (gTexture != nullptr) {
-        SDL_DestroyTexture(gTexture);
-        gTexture = nullptr;
-    }
-    if (gRenderer != nullptr) {
-        SDL_DestroyRenderer(gRenderer);
-        gRenderer = nullptr;
-    }
-    if (gWindow != nullptr) {
-        SDL_DestroyWindow(gWindow);
-        gWindow = nullptr;
-    }
-    IMG_Quit();
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
     SDL_Quit();
+}
+
+void handleCollision() {
+    if (playerRect.x < objectRect.x + OBJECT_SIZE &&
+        playerRect.x + PLAYER_SIZE > objectRect.x &&
+        playerRect.y < objectRect.y + OBJECT_SIZE &&
+        playerRect.y + PLAYER_SIZE > objectRect.y) {
+
+        if (!isColliding) {
+            playerHealth -= 10;
+            std::cout << "플레이어 체력: " << playerHealth << std::endl;
+            isColliding = true;
+
+            // 추가: 돌진 중이라면 돌진 상태 해제
+            isDashing = false;
+        }
+    }
+    else if (isColliding) {
+        isColliding = false;
+    }
+}
+
+void handleInput() {
+    SDL_Event e;
+    while (SDL_PollEvent(&e) != 0) {
+        if (e.type == SDL_QUIT) {
+            closeSDL();
+            exit(0);
+        }
+        else if (e.type == SDL_KEYDOWN) {
+            switch (e.key.keysym.sym) {
+            case SDLK_UP:
+                playerRect.y -= isDashing ? DASH_SPEED : 20;
+                break;
+            case SDLK_DOWN:
+                playerRect.y += isDashing ? DASH_SPEED : 20;
+                break;
+            case SDLK_LEFT:
+                playerRect.x -= isDashing ? DASH_SPEED : 20;
+                break;
+            case SDLK_RIGHT:
+                playerRect.x += isDashing ? DASH_SPEED : 20;
+                break;
+            case SDLK_SPACE:
+                // 스페이스바를 누르면 돌진
+                isDashing = true;
+                // 돌진 방향 설정 
+                dashDirectionX = (e.key.keysym.sym == SDLK_LEFT) ? -1 : (e.key.keysym.sym == SDLK_RIGHT) ? 1 : 0;
+                dashDirectionY = (e.key.keysym.sym == SDLK_UP) ? -1 : (e.key.keysym.sym == SDLK_DOWN) ? 1 : 0;
+                break;
+            }
+        }
+        else if (e.type == SDL_KEYUP) {
+            if (e.key.keysym.sym == SDLK_SPACE) {
+                // 스페이스바를 떼면 돌진 상태 해제
+                isDashing = false;
+            }
+        }
+    }
+
+    // 스페이스바를 누르고 있을 때 주변 색상을 빨간색으로 변경
+    if (isDashing) {
+        Uint8 r, g, b, a;
+        SDL_GetRenderDrawColor(gRenderer, &r, &g, &b, &a);
+
+        // 빨간색으로 점차 변경
+        r = std::min<Uint8>(255, r + 5);
+        g = std::max<Uint8>(0, g - 5);
+        b = std::max<Uint8>(0, b - 5);
+
+        SDL_SetRenderDrawColor(gRenderer, r, g, b, a);
+    }
+
+}
+
+void renderPlayerHealth() {
+    SDL_Rect healthBarRect = { SCREEN_WIDTH - 120, 20, static_cast<int>(100 * (static_cast<float>(playerHealth) / 100)), 20 };
+    SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(gRenderer, &healthBarRect);
 }
 
 int main(int argc, char* args[]) {
     if (!initSDL()) {
-        cerr << "Failed to initialize SDL." << endl;
         return -1;
     }
-    if (!loadMedia("assets/temp_start.png")) {
-        cerr << "Failed to load media." << endl;
-        return -1;
-    }
-    bool quit = false;
-    SDL_Event e;
-    while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            }
-            else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_SPACE) {
-                // 스페이스 키를 누를 때 다음 화면으로 전환합니다.
-                if (!loadMedia("assets/temp_next.png")) {
-                    cerr << "Failed to load media." << endl;
-                    return -1;
-                }
-            }
-        }
+
+    while (true) {
+        std::cout << isDashing;
+        Uint32 frameStart = SDL_GetTicks();
+
+        handleInput();
+        handleCollision();
+
+        SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 255);
         SDL_RenderClear(gRenderer);
-        SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+
+        SDL_SetRenderDrawColor(gRenderer, 0, 0, 255, 255);
+        SDL_RenderFillRect(gRenderer, &playerRect);
+
+        SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(gRenderer, &objectRect);
+
+        renderPlayerHealth();
+
         SDL_RenderPresent(gRenderer);
+
+        Uint32 frameTime = SDL_GetTicks() - frameStart;
+        if (frameTime < 16) {
+            SDL_Delay(16 - frameTime);
+        }
     }
+
     closeSDL();
     return 0;
 }
